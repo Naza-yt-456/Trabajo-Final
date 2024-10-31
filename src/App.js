5962d805b22c1f16641b12e5148788b70d8f0b17
Dan <127danigarcia@gmail.com>
2024-10-31 16:02:49 -0300

    app base

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import React from "react";
+import Turismo from "./componentes/turismo";
+import { Route, Routes } from "react-router-dom";
+import News from "./routes/News";
+import Home from "./routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/News" element={<News />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
